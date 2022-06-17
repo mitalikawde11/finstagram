7aa924738106ecce80b2db15af1df94b6a092a53
mitalikawde11 <mitali.kawde11@gmail.com>
2022-06-17 00:35:54 +0000

    add get and post actions for '/signup'

--- app/actions.rb
+++ app/actions.rb
@@ -4,9 +4,34 @@
 
 
 get '/' do
-  
   @finstagram_posts = FinstagramPost.order(created_at: :desc)  
   erb(:index)
-
 end 
+
+get '/signup' do
+  @user = User.new()
+  erb(:signup)
+end
+
+post '/signup' do
+  #grab user input values from params
+  email = params[:email]
+  avatar_url = params[:avatar_url]
+  username = params[:username]
+  password = params[:password]
+
+  #instate and save a user
+  @user = User.new({email: email, avatar_url: avatar_url, username: username, password: password})
+  
+  # if user validation pass and user is saved
+  if @user.save
+    "User #{username} saved!"
+    
+  else
+    erb(:signup)
+
+  end
+  
+end  
+
 
